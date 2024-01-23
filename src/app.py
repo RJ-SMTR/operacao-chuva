f4783f31fa304744f1a6bb19b53c5e30481893f7
Boris Marinho <marinho.boris@gmail.com>
2024-01-23 12:02:23 -0300

    define algumas versoes de dependencias

--- src/app.py
+++ src/app.py
@@ -452,7 +452,8 @@ def main():
     folium.LayerControl().add_to(m)
 
     print(">>> AQUI 10:", datetime.now())
-    map_data = st_folium(m, key="mapa", height=600, width=1200)
+    # map_data = st_folium(m, key="mapa", height=600, width=1200)
+    m.render()
 
 if __name__ == '__main__':
   main()
